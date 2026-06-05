03e7606e9ea4147a91c26c9b922c5ad61d06ba1d
Nagamma <nagamma06@gmail.com>
2026-06-05 23:51:45 +0530

    optimized extraction test code by including conftest file and fixtures

---
+++ conftest.py
@@ -0,0 +1,49 @@
+import logging
+import os
+import pytest
+
+from sqlalchemy import create_engine
+from test_configuration.etlTestConfig import *
+
+@pytest.fixture
+def get_logger():
+    #fixture factory (fixture returning a function).
+    def _get_logger(logger_name,log_file):
+        logger = logging.getLogger(logger_name)
+        # Prevent duplicate handlers
+        if logger.hasHandlers():
+            logger.handlers.clear()
+
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler = logging.FileHandler(os.path.join('logs',log_file),mode='a')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        return logger
+    return _get_logger
+
+@pytest.fixture
+def get_mysql_database_connection(get_logger):
+    logger = get_logger("db_logger","database.log")
+    logger.info('Connecting to MySQL database...')
+    try:
+        mysql_engine = create_engine(f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}').connect()
+        logger.info('MySQL database connected.')
+        yield mysql_engine
+        mysql_engine.close()
+        logger.info('MySQL database closed.')
+    except Exception as e:
+        logger.error("Error in db connection ", e, exc_info=True)
+
+@pytest.fixture
+def get_oracle_database_connection(get_logger):
+    logger = get_logger("db_logger", "database.log")
+    logger.info('Connecting to Oracle database...')
+    try:
+        oracle_engine = create_engine(f'oracle+oracledb://{ORACLE_USER}:{ORACLE_PASSWORD}@localhost:{ORACLE_PORT}/?service_name={ORACLE_SERVICE}').connect()
+        logger.info('Oracle database connected.')
+        yield oracle_engine
+        oracle_engine.close()
+        logger.info('Oracle database closed.')
+    except Exception as e:
+        logger.error("Error in db connection ", e, exc_info=True)
--- test_scripts/test_data_extraction.py
+++ test_scripts/test_data_extraction.py
@@ -1,31 +1,28 @@
 import pandas as pd
 import pytest
-from common_utilities.db_connection import mysql_engine
-from test_configuration.logger_config import setup_logger
 
-@pytest.fixture
-def logger():
-    return setup_logger("extraction_logger", "extraction.log")
-
+@pytest.mark.usefixtures("get_mysql_database_connection","get_logger")
 class TestDataExtraction:
-    def test_compare_supplier_json_and_stage_supplier_data(self,logger):
+    def test_compare_supplier_json_and_stage_supplier_data(self,get_mysql_database_connection,get_logger):
+        logger = get_logger("extraction_logger", "extraction.log")
         logger.info("Supplier data comparison test started")
         try:
             df_expected = pd.read_json('test_data/supplier_data.json')
             query_actual = """select * from stag_suplier"""
-            df_actual = pd.read_sql(query_actual,mysql_engine)
+            df_actual = pd.read_sql(query_actual,get_mysql_database_connection)
             assert df_actual.equals(df_expected),"Supplier data did not extract correctly"
             logger.info("Test Passed: Supplier JSON data matched with stage_supplier table data")
         except Exception as e:
             logger.error(f"Test failed due to error :{e}",exc_info=True)
             pytest.fail(f"Test execution failed :{e}")
 
-    def test_compare_inventory_xml_and_stage_inventory_data(self,logger):
+    def test_compare_inventory_xml_and_stage_inventory_data(self,get_mysql_database_connection,get_logger):
+        logger = get_logger("extraction_logger", "extraction.log")
         logger.info(f"Inventory data comparison test started")
         try:
             df_expected = pd.read_xml('test_data/inventory_data.xml',parser='etree',xpath=".//item")
             query_actual = """select * from stag_inventory"""
-            df_actual = pd.read_sql(query_actual,mysql_engine)
+            df_actual = pd.read_sql(query_actual,get_mysql_database_connection)
             assert df_actual.equals(df_expected), "Inventory data did not extract correctly"
             logger.info("Test Passed: Inventory data matched with stage_inventory table data")
         except Exception as e:
